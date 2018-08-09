ca7d144eba34216ea5610808a0a80f1747928bc3
liruizi <31061595+liruizi@users.noreply.github.com>
2018-08-09 17:17:41 +0800

    项目测试

---
+++ src/main/java/com/qiubit/demo/QiubitApplication.java
@@ -0,0 +1,12 @@
+package com.qiubit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiubitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QiubitApplication.class, args);
+	}
+}
