89457c7febd54c4aa9f14d5e00e26b5fa000d2f8
liruizi <31061595+liruizi@users.noreply.github.com>
2018-08-24 16:16:34 +0800

    测试

--- qiubit-web/src/main/java/com/qiubit/QiubitApplication.java
+++ qiubit-web/src/main/java/com/qiubit/QiubitApplication.java
@@ -1,10 +1,15 @@
 package com.qiubit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QiubitApplication {
+
+	private final Logger logger =  LoggerFactory.getLogger(QiubitApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(QiubitApplication.class, args);
 	}
