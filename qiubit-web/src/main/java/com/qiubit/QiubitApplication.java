d463be6804bcd5a7839c8bd66fba4dff8b915078
liruizi <31061595+liruizi@users.noreply.github.com>
2018-08-24 16:41:14 +0800

    测试

--- qiubit-web/src/main/java/com/qiubit/QiubitApplication.java
+++ qiubit-web/src/main/java/com/qiubit/QiubitApplication.java
@@ -4,11 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class QiubitApplication {
 
-	private final Logger logger =  LoggerFactory.getLogger(QiubitApplication.class);
+	private final Logger logger = LoggerFactory.getLogger(QiubitApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(QiubitApplication.class, args);
--- qiubit-web/src/main/java/com/qiubit/controller/test.java
+++ qiubit-web/src/main/java/com/qiubit/controller/test.java
@@ -1,10 +1,14 @@
 package com.qiubit.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class test {
+	private final Logger logger = LoggerFactory.getLogger(test.class);
+
 	/**
 	 * 登录跳转 https://www.jianshu.com/p/8d2cc7207fb2
 	 * 
@@ -13,6 +17,7 @@ public class test {
 	 */
 	@GetMapping("/admin/login")
 	public String loginGet(Model model) {
+		logger.info("kais kais ");
 		return "login";
 	}
 }
