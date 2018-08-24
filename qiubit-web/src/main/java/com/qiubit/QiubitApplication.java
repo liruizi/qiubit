85bf010517e19395d7a7c261693df1c5a5963447
liruizi <31061595+liruizi@users.noreply.github.com>
2018-08-24 17:10:42 +0800

    cccccc

--- qiubit-web/src/main/java/com/qiubit/QiubitApplication.java
+++ qiubit-web/src/main/java/com/qiubit/QiubitApplication.java
@@ -1,16 +1,13 @@
 package com.qiubit;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 //@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class QiubitApplication {
 
-	private final Logger logger = LoggerFactory.getLogger(QiubitApplication.class);
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(QiubitApplication.class, args);
